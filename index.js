3ed005756b56fe92400f304a2826a052c33fdd13
Parzanga <54054900+Parzanga@users.noreply.github.com>
2022-04-07 01:57:22 +0400

    Update index.js
    Removed map function

--- index.js
+++ index.js
@@ -168,9 +168,6 @@ function START() {
         }
     }
 
-    function map(value, start1, stop1, start2, stop2) {
-        return (value - start1) / (stop1 - start1) * (stop2 - start2) + start2;
-    }
     class Ball {
         constructor(x, y, radius) {
             this.velocity = Vector.random2D(Math.random() * Math.PI);
